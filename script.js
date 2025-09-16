d7bd819e4bf5836d61e25a3bb5fa7e8f2e81b3ed
dkhayutin <dan.khayutin@gmail.com>
2025-09-16 11:05:04 -0400

    linked script file, added first div

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-function checkAge(num){
-  if (num > 21) {
-    return ('You may enter the bar')
-  } else if  (num < 21) {
-    return ('You are to young')
-  } else {
-    return ('No age given')
-  }
-}
-
-console.log(checkAge(20))
+alert('TEST')
