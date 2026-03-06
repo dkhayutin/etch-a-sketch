9acce46c9223783ee124416cd199d647bdfc3de1
dkhayutin <dan.khayutin@gmail.com>
2026-03-06 07:37:32 -0500

    refresh

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-alert('TEST')
+// let container = document.querySelector("#containter")
+
+// function createRow () {
+// for (let i = 0; i < 256; i++) {
+// newDiv = document.createElement("div")
+// container.appendChild(newDiv).style.backgroundColor = "yellow"
+// }
+// }
+
+// createRow()
+
+const date = new Date().toDateString(); 
+
+console.log(date)
