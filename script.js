b46f3cfe4f49ed41e515cc6e848bb68f10a7e50c
dkhayutin <dan.khayutin@gmail.com>
2026-03-08 16:03:45 -0400

    created 16x16 grid

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 const container = document.querySelector('#container')
 
-for (let i = 1; i < 256; i++) {
-  let div = document.createElement('div')
-  div.style.cssText = "border : 1px solid blue; height : 20px; padding : 10px; margin : 10px "
-  div.classList.add('divSquare')
-  container.appendChild(div)
-}
+  function makeGrid() {
+  for (let i = 1; i <= (256); i++) {
+    let div = document.createElement('div')
+    div.classList.add('divSquare')
+    container.appendChild(div)
+  }
+  }
+
+  makeGrid()
 
 
