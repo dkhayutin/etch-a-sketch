4eaad2779b5bc55a444ee8fadca8b47261488a7f
dkhayutin <dan.khayutin@gmail.com>
2026-03-16 08:04:46 -0400

    added clear button to remove existing grid

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 const container = document.querySelector('#container')
 const btn = document.querySelector('#newGrid')
 btn.addEventListener("click", makeGrid)
+const removeBtn = document.querySelector('#removeGrid')
+removeBtn.addEventListener("click", () => {
+  container.innerHTML = ""; 
+})
 
-
+ 
   function makeGrid() {
     let num = prompt('Please enter a number between 2 and 100')
-     if(num <2 || num > 100){
-      alert('The number must be between 2 and 100')
-     }
+     if(num < 2 || num > 100){
+      alert ('Number not valid')
+      } else {
     const squareSize = 640/num
   for (let i = 1; i <= num *num ; i++) {
     const div = document.createElement('div')
@@ -16,10 +20,7 @@ btn.addEventListener("click", makeGrid)
      div.style.height = `${squareSize}px`
     container.appendChild(div)
     }
-  } 
-
-
-
+  }
 
   const colorDiv = document.querySelectorAll('.divSquare')
 colorDiv.forEach(div => 
@@ -28,4 +29,8 @@ colorDiv.forEach(div =>
 
   })
 )
-  
+}
+
+
+
+
